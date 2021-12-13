46e44fa81957afcfbdf8995059aa51aa7bc31361
rmn-smn <94612766+rmn-smn@users.noreply.github.com>
2021-12-13 14:00:49 +0100

    Update image_detect_coco.py

--- image_detect_coco.py
+++ image_detect_coco.py
@@ -11,8 +11,8 @@ coco_classes = "model_data/coco/coco.names"
 darknetweights = "model_data/yolov4.weights"
 
 model = YOLOv4(classes=coco_classes)
-initializer = Initializer(model)
-initializer.load_darknet_weights(darknetweights = darknetweights)
+initializer = Initializer(model,darknetweights = darknetweights)
+initializer.load_darknet_weights()
 
 detect_image(model, image_path, "", classes=coco_classes, input_size=416, 
              show=True, rectangle_colors=(255,0,0))
